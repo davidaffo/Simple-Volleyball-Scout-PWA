e6c0a9815dc10f35e5d4aadf3cf999b1fce595d9
davidaffo <davidedaffo@gmail.com>
2026-02-10 17:35:09 +0100

    interface code refactoring

--- js/app-version.js
+++ js/app-version.js
@@ -1,11 +1,11 @@
 (function attachAppVersion(root) {
   root.__APP_VERSION__ = {
   "appName": "Simple Volleyball Scout PWA",
-  "baseVersion": "0.3.13",
-  "version": "0.3.13+281.39b5ec6",
-  "commitCount": 281,
-  "commitHash": "39b5ec6",
-  "cacheVersion": "v281-39b5ec6",
-  "buildDate": "2026-02-10T10:56:20.604Z"
+  "baseVersion": "0.3.14",
+  "version": "0.3.14+285.f1b7ffd",
+  "commitCount": 285,
+  "commitHash": "f1b7ffd",
+  "cacheVersion": "v285-f1b7ffd",
+  "buildDate": "2026-02-10T16:34:57.682Z"
 };
 })(typeof self !== "undefined" ? self : window);
