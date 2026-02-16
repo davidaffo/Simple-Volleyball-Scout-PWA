84af0326a555f81374782b9aaddd118f2b68a783
davidaffo <davidedaffo@gmail.com>
2026-02-16 11:16:39 +0100

    filter presets completely implemented

--- js/app-version.js
+++ js/app-version.js
@@ -1,11 +1,11 @@
 (function attachAppVersion(root) {
   root.__APP_VERSION__ = {
   "appName": "Simple Volleyball Scout PWA",
-  "baseVersion": "0.3.14",
-  "version": "0.3.14+285.f1b7ffd",
-  "commitCount": 285,
-  "commitHash": "f1b7ffd",
-  "cacheVersion": "v285-f1b7ffd",
-  "buildDate": "2026-02-10T16:34:57.682Z"
+  "baseVersion": "0.4.0",
+  "version": "0.4.0+289.eaf40af",
+  "commitCount": 289,
+  "commitHash": "eaf40af",
+  "cacheVersion": "v289-eaf40af",
+  "buildDate": "2026-02-16T10:16:34.999Z"
 };
 })(typeof self !== "undefined" ? self : window);
